b910e0b501b9aac60589a5ac979c7864446193a7
Tijl Grootswagers <t.grootswagers@gmail.com>
2021-06-02 10:29:39 +1000

    Add files via upload

--- codes/local_functions/bayesfactor_R_wrapper.m
+++ codes/local_functions/bayesfactor_R_wrapper.m
@@ -13,29 +13,37 @@ function bf = bayesfactor_R_wrapper(data,varargin)
     % Inputs:
     %   data            PxR data for P tests and R observations (subjects)
     % Optional inputs:
-    %   'Rpath',p       a string with the path to Rscript. Use 'which Rscript'
+    %   'Rpath',p       String with the path to Rscript. Use 'which Rscript'
     %                   in a unix terminal to find this path. This should
     %                   not be the path to R (default '/usr/local/bin/Rscript')
-    %   'verbose',v     if true, print the output from R, recommended to check
+    %   'verbose',v     If true, print the output from R, recommended to check
     %                   that everything works as intended (default false).
-    %   'prior',p       string defining the prior type, one of 'cauchy' (default) or 'uniform'
-    %   'lower',i       for a uniform prior, the lower bound of the interval (default: 0)
-    %   'upper',i       for a uniform prior, the upper bound of the interval (default: 10)
-    %   'args',a        for a cauchy prior, a string with arguments to pass to ttestBF in R
-    %                   must be valid R code (default ''). Examples:
-    %                   - standard JZS medium-width cauchy prior (same as default):
-    %                     'mu=0,rscale="medium"'
-    %                   - medium-width half-cauchy with a null-interval on 
-    %                     the effect size (use with returnindex 2):
-    %                     'mu=0,rscale="medium",nullInterval=c(-Inf,0.5)'
-    %                   - two-sided medium-width half-cauchy with a null-interval 
-    %                     on the effect size (use with returnindex 2):
-    %                     'mu=0,rscale="medium",nullInterval=c(-0.5,0.5)'
-    %   'returnindex',r for a cauchy prior, which BF to return, can be either 1 or 2 (default 1).
-    %                   a value of 2 returns the complementary bf (e.g. for use with a null-interval)
+    %   'args',a        String with arguments to pass to ttestBF in R.
+    %                   Must be valid R code (default '').
+    %                   Examples:
+    %                   - standard JZS medium-width cauchy prior (default):
+    %                       'mu=0,rscale="medium"'
+    %                   - medium-width one-sided half-cauchy:
+    %                       'mu=0,rscale="medium",nullInterval=c(0,Inf)'
+    %                   - medium-width one-sided half-cauchy specifying an 
+    %                     interval defined on the alternative:
+    %                       'mu=0,rscale="medium",nullInterval=c(0.5,Inf)'
+    %                   - two-sided medium-width half-cauchy with an interval 
+    %                     defined on the alternative (use with returnindex 2):
+    %                       'mu=0,rscale="medium",nullInterval=c(-0.5,0.5)'
+    %   'tempdir'       A string defining the path to store temporary files.
+    %                   This function uses temporary files to pass to R.
+    %                   Use this when calling this function in parallel.
+    %   'returnindex',r BF to return, can be either 1 or 2 (default 1).
+    %                   a value of 2 returns the bf for the complementary 
+    %                   interval when specifying an interval hypothesis
     % Returns:
     %   bf              Px1 vector with Bayes factors
     % 
+    % Example use of this function:
+    % X = randn(1,20) % random data (20 observations)
+    % bf = bayesfactor_R_wrapper(X,'args','nullInterval=c(0.5,Inf)','verbose',1)
+    %
     % For more example uses and interpretation, see ttestBF documentation at
     % https://www.rdocumentation.org/packages/BayesFactor/versions/0.9.12-4.2/topics/ttestBF
     % https://richarddmorey.github.io/BayesFactor/#onesample
@@ -50,9 +58,6 @@ function bf = bayesfactor_R_wrapper(data,varargin)
     opt.Rpath='/usr/local/bin/Rscript';
     opt.returnindex=1;
     opt.args='';
-    opt.prior='cauchy';
-    opt.lower = 0;
-    opt.upper = 10;
     opt.verbose=false;
     opt.tempdir='';
     % read input key-value pairs and overwrite the default values
@@ -66,9 +71,9 @@ function bf = bayesfactor_R_wrapper(data,varargin)
     
     %% check inputs
     assert(ischar(opt.Rpath),'value for ''Rpath'' must be a character array')
+    assert(ischar(opt.tempdir),'value for ''tempdir'' must be a character array')
     assert(ischar(opt.args),'value for ''args'' must be a character array')
     assert(ismember(opt.returnindex,[1 2]),'value for ''returnindex'' must be 1 or 2')
-    assert(ismember(opt.prior,{'cauchy','uniform'}),'value for prior must be one of ''cauchy'' or ''uniform''')
     
     %% test if calling Rscript
     cmd = sprintf('which %s',opt.Rpath);
@@ -90,45 +95,24 @@ function bf = bayesfactor_R_wrapper(data,varargin)
     bfstatsoutfn = [bfstatstempdir 'out.csv'];
     bfscriptfn = [bfstatstempdir 'bf_fun.r'];
         
-    % write data to csv file
+    % write data to temporary csv file
     %writematrix(data,bfstatsinfn);
     writetable(array2table(data),bfstatsinfn,'WriteVariableNames',0);
     
-    %% 
-    if strcmp(opt.prior,'cauchy')
-        %% cauchy prior
-        fid = fopen(bfscriptfn,'w');
-        fprintf(fid,['library("BayesFactor")\n'...
-            'X = as.matrix(read.table("%s",header=FALSE,sep=","))\n'...
-            'bf10 = numeric(length = nrow(X))\n'...
-            'for (row in 1:nrow(X)) {data = as.numeric(X[row,])\n'...
-            'bf = ttestBF(x=data,%s)\n'...
-            'print(bf)\n',...
-            'bf10[row] = as.vector(bf[%i])}\n'...
-            'write.table(bf10,"%s",sep=",",row.names=FALSE,col.names="BF10")\n'],...
-            bfstatsinfn,opt.args,opt.returnindex,bfstatsoutfn);
-        fclose(fid);
-    elseif strcmp(opt.prior,'uniform')
-        %% bounded uniform prior
-        fid = fopen(bfscriptfn,'w');
-        fprintf(fid,['X = as.matrix(read.table("%s",header=FALSE,sep=","))\n'...
-            'bf10 = numeric(length = nrow(X))\n'...
-            'lower = %d\n'...
-            'upper = %d\n'...
-            'for (row in 1:nrow(X)) {data = as.numeric(X[row,])\n'...
-            'neff = length(data)\n'...
-            't = mean(data)/(sd(data)/sqrt(neff))\n'...
-            'df = neff-1\n'...
-            'mlike = integrate(function(d) dt(t, df, d * sqrt(neff))/abs(upper-lower),lower = lower,upper = upper)[[1]]\n'...
-            'bf = mlike / dt(t, df)\n'...
-            'print(bf)\n'...
-            'bf10[row] = as.vector(bf)}\n'...
-            'write.table(bf10,"%s",sep=",",row.names=FALSE,col.names="BF10")\n'],...
-            bfstatsinfn,opt.lower,opt.upper,bfstatsoutfn);
-        fclose(fid);
-    end
+    % write temporary R script that runs the BF with a cauchy prior
+    fid = fopen(bfscriptfn,'w');
+    fprintf(fid,['library("BayesFactor")\n'...
+        'X = as.matrix(read.table("%s",header=FALSE,sep=","))\n'...
+        'bf10 = numeric(length = nrow(X))\n'...
+        'for (row in 1:nrow(X)) {data = as.numeric(X[row,])\n'...
+        'bf = ttestBF(x=data,%s)\n'...
+        'print(bf)\n',...
+        'bf10[row] = as.vector(bf[%i])}\n'...
+        'write.table(bf10,"%s",sep=",",row.names=FALSE,col.names="BF10")\n'],...
+        bfstatsinfn,opt.args,opt.returnindex,bfstatsoutfn);
+    fclose(fid);
     
-    %%
+    %% call R and read results from the temporary file that R created
     cmd = sprintf('%s %s',opt.Rpath,bfscriptfn);
     [status,output]=system(cmd);
     if opt.verbose
